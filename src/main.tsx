5f27646b0c5fb57e5acc87561b850f191c176e63
rs <git@rsoo.io>
2026-05-26 18:31:12 +0800

    build2

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { routeTree } from './routeTree.gen'
 
 const router = createRouter({
   routeTree,
+  basepath: import.meta.env.BASE_URL.replace(/\/$/, ''),
   defaultPreload: 'intent',
   scrollRestoration: true,
 })
